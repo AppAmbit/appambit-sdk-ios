5187742d1e8fc7c9d704e4112fd607354c20a891
Isaias Canales <jesus@kavaup.io>
2025-07-03 14:32:07 -0600

    chore(library): the library for ios was created

--- AppAmbitTestApp/AppAmbitTestingApp.swift
+++ AppAmbitTestApp/AppAmbitTestingApp.swift
@@ -1,7 +1,11 @@
 import SwiftUI
+import AppAmbit;
 
 @main
 struct AppAmbitTestingApp: App {
+    init() {
+        AppAmbit.start()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
