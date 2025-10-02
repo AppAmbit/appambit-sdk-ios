fbeb231a3992a38c8497632f0a2a55a6f1708e87
Isaias Canales <jesus@kavaup.io>
2025-10-02 11:55:13 -0600

    chore: test

--- AppAmbitTestApp/AppAmbitTestingApp.swift
+++ AppAmbitTestApp/AppAmbitTestingApp.swift
@@ -6,7 +6,7 @@ struct AppAmbitTestingApp: App {
     init() {
         //Uncomment the line for manual session management
         //Analytics.enableManualSession()
-        AppAmbit.start(appKey: "<YOUR-APPKEY>")                
+        AppAmbit.start(appKey: "3f9c8cba-1628-40b4-93ae-6dbba53210c3")                
     }
     var body: some Scene {
         WindowGroup {
