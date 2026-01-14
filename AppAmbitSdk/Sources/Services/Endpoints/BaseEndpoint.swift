09c20e167f9b29c50fd52b201f39fde02ef9e33b
Isaias Canales <jesus@kavaup.io>
2026-01-14 13:47:03 -0600

    fix: change url for web services

--- AppAmbitSdk/Sources/Services/Endpoints/BaseEndpoint.swift
+++ AppAmbitSdk/Sources/Services/Endpoints/BaseEndpoint.swift
@@ -1,6 +1,6 @@
 class BaseEndpoint: Endpoint {
     var url: String = ""
-    var baseUrl: String = "https://staging-appambit.com/api"
+    var baseUrl: String = "https://appambit.com/api"
     var payload: Any?
     var method: HttpMethodApp = .get
     var customHeader: [String: String]?
